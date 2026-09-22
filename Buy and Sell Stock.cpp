cf357952f29b265ccc9c258f557bf0bf6054f2b4
Yuvraj4518-tag <123yuvigaur@gmail.com>
2026-09-22 22:15:17 +0530

    Update Buy and Sell Stock.cpp
    better time complexity than previous.

--- Buy and Sell Stock.cpp
+++ Buy and Sell Stock.cpp
@@ -1,20 +1,27 @@
 best time to buy stock and sell it
 
-#include <bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+#include <climits> // For INT_MAX
+
 using namespace std;
 
-void buyStocks(vector<int> &arr, int n) {
-    
-    int MaxProfit = 0;
-    int bestBuy = arr[0];
-    for (int i =1; i<n; i++) {
-        if (arr[i] > bestBuy) {
-            MaxProfit = max(MaxProfit, arr[i]-bestBuy);
-        }
-        bestBuy = min(bestBuy, arr[i]);
+int maxProfit(vector<int>& prices) {
+    int min_price = INT_MAX; // Keep track of the lowest buying price
+    int max_profit = 0;      // Keep track of the highest profit
 
+    for (int i = 0; i < prices.size(); i++) {
+        if (prices[i] < min_price) {
+            // Update min_price if we find a cheaper day to buy
+            min_price = prices[i];
+        } else {
+            // Calculate profit if we sold today, update max_profit if it's higher
+            int current_profit = prices[i] - min_price;
+            max_profit = max(max_profit, current_profit);
+        }
     }
-   cout << MaxProfit << endl;
+
+    return max_profit;
 }
 
 int main () {
