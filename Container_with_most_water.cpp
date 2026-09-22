082d6b556087dbd196af7848e884566ba6bcbb71
Yuvraj4518-tag <123yuvigaur@gmail.com>
2026-09-22 22:21:44 +0530

    Update Container_with_most_water.cpp
    update with adding comments.

--- Container_with_most_water.cpp
+++ Container_with_most_water.cpp
@@ -1,24 +1,38 @@
-// Water container question
-
-
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        // Track the maximum water area found so far
         int maxWater = 0;
         int n = height.size();
-        int lp = 0, rp= n-1;
-        while (lp<rp) {
+        
+        // Initialize two pointers: one at the start (left) and one at the end (right)
+        int lp = 0, rp = n - 1;
+        
+        // Loop until the two pointers meet
+        while (lp < rp) {
+            // The width of the current container is the distance between the two pointers
             int wid = rp - lp;
+            
+            // The water level is limited by the shorter line, so find the minimum height
             int ht = min(height[lp], height[rp]);
-            int area = wid*ht;
-            maxWater = max (maxWater, area);
+            
+            // Calculate the area of the current container
+            int area = wid * ht;
+            
+            // Update maxWater if the current container holds more water
+            maxWater = max(maxWater, area);
+            
+            // Move the pointer that points to the shorter line inward.
+            // We do this because keeping the shorter line and moving the taller one 
+            // will only decrease the width without increasing the possible height.
             if(height[lp] < height[rp]) {
                 lp++;
             } else {
                 rp--;
             }
-        
         }
+        
+        // Return the maximum area found after checking all potential containers
         return maxWater;
     }
 };
