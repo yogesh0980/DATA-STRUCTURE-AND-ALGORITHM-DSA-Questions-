f76845708d0d2d30f294fb7a27944f072c5351ed
Yuvraj4518-tag <123yuvigaur@gmail.com>
2026-09-24 21:27:43 +0530

    Update Combination_Sum.cpp
    correction

--- Combination_Sum.cpp
+++ Combination_Sum.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-    int maxArea(vector<int>& height) {
-        // Track the maximum water area found so far
-        int maxWater = 0;
-        int n = height.size();
-        
-        // Initialize two pointers: one at the start (left) and one at the end (right)
-        int lp = 0, rp = n - 1;
-        
-        // Loop until the two pointers meet
-        while (lp < rp) {
-            // The width of the current container is the distance between the two pointers
-            int wid = rp - lp;
-            
-            // The water level is limited by the shorter line, so find the minimum height
-            int ht = min(height[lp], height[rp]);
-            
-            // Calculate the area of the current container
-            int area = wid * ht;
-            
-            // Update maxWater if the current container holds more water
-            maxWater = max(maxWater, area);
-            
-            // Move the pointer that points to the shorter line inward.
-            // We do this because keeping the shorter line and moving the taller one 
-            // will only decrease the width without increasing the possible height.
-            if(height[lp] < height[rp]) {
-                lp++;
-            } else {
-                rp--;
-            }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> result;
+        vector<int> current;
+
+        sort(candidates.begin(), candidates.end());
+
+        backtrack(0, target, candidates, current, result);
+        return result;
+    }
+
+private:
+    void backtrack(int startIdx, int remaining, const vector<int>& candidates, vector<int>& current, vector<vector<int>>& result) {
+        if (remaining == 0) {
+            result.push_back(current);
+            return;
         }
-        
-        // Return the maximum area found after checking all potential containers
-        return maxWater;
+
+        for (int i = startIdx; i < candidates.size(); ++i) {
+            if (candidates[i] > remaining) {
+                break;
+            }
+            current.push_back(candidates[i]);
+            backtrack(i, remaining - candidates[i], candidates, current, result);
+            current.pop_back();
+        }
     }
 };
